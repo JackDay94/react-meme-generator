c72283e1f958bbbe4e66b3d13cd723e46a3dc4cb
Jack Day <jack94day@gmail.com>
2025-02-27 17:27:43 +0000

    Add Main content and styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,22 @@
 export default function Main() {
     return (
         <main>
-            <h1>Main Content</h1>
+            <div className="form">
+                <label>Top Text
+                    <input type="text" name="topText" placeholder="One does not simply"/>
+                </label>
+
+                <label>Bottom Text
+                    <input type="text" name="bottomText" placeholder="Walk into Mordor"/>
+                </label>
+
+                <button>Get a new meme image</button>
+            </div>
+            <div className="meme">
+                <img src="http://i.imgflip.com/1bij.jpg" />
+                <span className="top">One does not simply</span>
+                <span className="bottom">Walk into Mordor</span>
+            </div>
         </main>
     )
 }
