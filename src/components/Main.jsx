daaef78564ca610dd656999edca038711fb88a2b
Jack Day <jack94day@gmail.com>
2025-02-28 19:49:11 +0000

    Add typing functionality for top text and bottom text on meme

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,52 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Main() {
-    const [meme, setMeme] = useState({
-        topText: "One does not simply",
-        bottomText: "Walk into Mordor",
-        imageUrl: "http://i.imgflip.com/1bij.jpg",
-    })
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
 
-    return (
-        <main>
-            <div className="form">
-                <label>Top Text
-                    <input type="text" name="topText" placeholder="One does not simply"/>
-                </label>
+  function handleChange(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({
+        ...prevMeme, 
+        [name]: value, 
+    }));
+  }
 
-                <label>Bottom Text
-                    <input type="text" name="bottomText" placeholder="Walk into Mordor"/>
-                </label>
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input
+            type="text"
+            name="topText"
+            placeholder="One does not simply"
+            onChange={handleChange}
+            value={meme.topText}
+          />
+        </label>
 
-                <button>Get a new meme image</button>
-            </div>
-            <div className="meme">
-                <img src={meme.imageUrl} />
-                <span className="top">{meme.topText}</span>
-                <span className="bottom">{meme.bottomText}</span>
-            </div>
-        </main>
-    )
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Walk into Mordor"
+            onChange={handleChange}
+            value={meme.bottomText}
+          />
+        </label>
+
+        <button>Get a new meme image</button>
+      </div>
+      <div className="meme">
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
 }
